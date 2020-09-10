4e27b7f7fc86df96ead6212ec4490625edaea879
ymamidi@uno.edu <ymamidi@uno.edu>
2020-09-10 15:08:49 -0500

    created data frame

--- try.py
+++ try.py
@@ -1,2 +1,10 @@
 import streamlit as st
-st.write('hello')
+import pandas as pd
+st.title('My first app')
+x= st.slider('x')
+st.write(x, 'squared is', x*x)
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))

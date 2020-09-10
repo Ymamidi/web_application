33997eb9eb301fd25f5158c03fdadc1d8f4ed0de
ymamidi@uno.edu <ymamidi@uno.edu>
2020-09-10 17:18:03 -0500

    unique tables updated

--- try.py
+++ try.py
@@ -2,15 +2,19 @@ import streamlit as st
 import pandas as pd
 
 st.title('My first app')
-x= st.slider('x')
-st.write(x, 'squared is', x*x)
-st.write("Here's our first attempt at using data to create a table:")
 
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+# IRD = 'C:\Personal Projects\Development\Web_app\web_application\iris_data.csv'
+# df = pd.read_csv(IRD, parse_dates=['Class'])
+# # st.write(df.loc[df['Class']=='versicolor'])
+df = pd.read_csv (r'C:\Personal Projects\Development\Web_app\web_application\iris_data.csv')
+# st.markdown("#### " +"What would you like to select?")
 
-st.write(pd.read_csv (r'C:\Personal Projects\Development\Web_app\web_application\genedata-transposed.csv'))
+# selected_metrics = st.selectbox(
+#     label="Choose...", options=['setosa','versicolor','virginica']
+# )
 
- 
+
+st.sidebar.title("Select your Class type")
+State_list = st.sidebar.selectbox('Class', df.Class.unique())
+st.write(df.loc[df['Class']==State_list])
+

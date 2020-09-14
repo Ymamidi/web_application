6cddfeaf64559c1dcefac302d879a1c7b84d45d6
ymamidi@uno.edu <ymamidi@uno.edu>
2020-09-13 19:20:37 -0500

    updated application

--- try.py
+++ try.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime
 
-st.title('My first app')
+st.title("Tissue Biorepository")
 
 # IRD = 'C:\Personal Projects\Development\Web_app\web_application\iris_data.csv'
 # df = pd.read_csv(IRD, parse_dates=['Class'])
@@ -15,25 +15,25 @@ df = pd.read_csv (r'C:\Personal Projects\Development\Web_app\web_application\Tis
 # )
 
 #selecting category
-st.title("Current searching")
-Type_list = st.selectbox('type', df.Type.unique())
+st.sidebar.title("Current searching")
+Type_list = st.sidebar.selectbox('type', df.Type.unique())
 df = (df.loc[df['Type']== Type_list])
 
 
 #selecting campus
-st.title("Campus selection")
-Campus_list = st.selectbox('campus', df.Campus.unique())
+st.sidebar.title("Campus selection")
+Campus_list = st.sidebar.selectbox('campus', df.Campus.unique())
 df = (df.loc[df['Campus']== Campus_list])
 
 #slecting type of specimen
-st.title("Select your Specimen type")
-Class_list = st.selectbox('Specimen', df.Specimen.unique())
+st.sidebar.title("Select your details")
+Class_list = st.sidebar.selectbox('Specimen', df.Specimen.unique())
 # Col_list = st.sidebar.selectbox("C", df.C.unique())
 df = (df.loc[df['Specimen']== Class_list])
 
 #selecting type of tissue
 Tissue = df['Type of tissue'].unique()
-tissues_SELECTED = st.multiselect('Select tissues', Tissue)
+tissues_SELECTED = st.sidebar.multiselect('Select tissues', Tissue)
 if tissues_SELECTED == []:
     pass 
 else:
@@ -41,7 +41,7 @@ else:
     
 #selecting preparation type
 P_type = df['Preperation'].unique()
-type_SELECTED = st.multiselect('Select type', P_type)
+type_SELECTED = st.sidebar.multiselect('Select type', P_type)
 if type_SELECTED == []:
     pass 
 else:
@@ -59,9 +59,31 @@ else:
 
 #selecting sex
 S_type = df['Sex'].unique()
-Sex_SELECTED = st.multiselect('Select sex', S_type)
+Sex_SELECTED = st.sidebar.multiselect('Select sex', S_type)
 if Sex_SELECTED == []:
     pass 
 else:
     df = df[df['Sex'].isin(Sex_SELECTED)]
+
+
+#selecting patient race
+R_type = df['Race'].unique()
+Race_SELECTED = st.sidebar.multiselect('Select race', R_type)
+if Race_SELECTED == []:
+    pass 
+else:
+    df = df[df['Race'].isin(Race_SELECTED)]
+
+
+#selecting Ethnicity
+E_type = df['Ethnicity'].unique()
+E_SELECTED = st.sidebar.multiselect('Select Ethnicity', E_type)
+if E_SELECTED == []:
+    pass 
+else:
+    df = df[df['Ethnicity'].isin(E_SELECTED)]
+
+
+st.subheader('Total Cases: ')
+st.write(len(df))
 st.write(df)
